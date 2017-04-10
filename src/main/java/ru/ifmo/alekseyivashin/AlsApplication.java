6a3f0041bcc754be24aaf60702ea04b3435c94b5
Aleksey Ivashin <alekseyivashin806@yandex.ru>
2017-04-10 23:11:19 +0300

    initial commit

---
+++ src/main/java/ru/ifmo/alekseyivashin/AlsApplication.java
@@ -0,0 +1,12 @@
+package ru.ifmo.alekseyivashin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlsApplication.class, args);
+	}
+}
